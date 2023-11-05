ab79a0d8ce32a8cd84e071b5a1da7eeb30f0e005
NaderTate <nadertate@gmail.com>
2023-11-05 16:51:27 +0200

    Update company name in footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -185,7 +185,7 @@ export default function Footer() {
         </div>
 
         <p className="text-sm text-gray-500">
-          &copy; 2023. Company Name. All rights reserved.
+          &copy; 2023. Excel Rank. All rights reserved.
         </p>
       </div>
     </footer>
